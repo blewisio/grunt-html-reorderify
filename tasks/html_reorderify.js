b97726bc51c0d2943d69b53e78abfabd02aa0d5c
Brent Lewis <brent.lewis08@gmail.com>
2014-01-25 12:48:21 -0600

    put core logic into new function

--- tasks/html_reorderify.js
+++ tasks/html_reorderify.js
@@ -8,6 +8,55 @@
 
 'use strict';
 
+function reorderAttributes(src, options) {
+  var i,
+      tagBeginIndex = null,
+      tagEndIndex = null;
+  for (i = 0; i < src.length; i++) {
+    if (tagBeginIndex === null) {
+      if (src[i] === '<') {
+        tagBeginIndex = i;
+      }
+    } else if (tagEndIndex === null) {
+      if (src[i] === '>') {
+        tagEndIndex = i;
+      }
+    } else {
+      var originalElement = src.substring(tagBeginIndex + 1, tagEndIndex);
+      if (originalElement[0] !== '/') {
+        var elementName = originalElement.substring(0, originalElement.indexOf(' '));
+        var element = originalElement.substring(originalElement.indexOf(' ') + 1);
+        var attributes = element.split(' ');
+        if (attributes.length > 1) {
+          var keyValuePairs = [];
+          var k;
+          for(k = 0; k < attributes.length; k++) {
+            var pair = attributes[k].split('=');
+            var obj = {
+                        'name': pair[0],
+                        'value': pair[1],
+                        'order': options.left.indexOf(pair[0])
+                      };
+            keyValuePairs.push(obj);
+          }
+          keyValuePairs.sort(function (a, b) {
+            return a.order - b.order;
+          });
+          var orderedElement = elementName;
+          var m;
+          for(m = 0; m < keyValuePairs.length; m++) {
+            orderedElement = orderedElement + ' ' + keyValuePairs[m].name + '=' + keyValuePairs[m].value;
+          }
+          src = src.replace(originalElement, orderedElement);
+        }
+      }
+      tagBeginIndex = null;
+      tagEndIndex = null;
+    }
+  }
+  return src;
+}
+
 module.exports = function(grunt) {
 
   // Please see the Grunt documentation for more information regarding task
@@ -21,52 +70,7 @@ module.exports = function(grunt) {
     var src = this.files[0].orig.src[0];
     if (grunt.file.exists(src)) {
       src = grunt.file.read(src);
-
-      var i,
-          tagBeginIndex = null,
-          tagEndIndex = null;
-      for (i = 0; i < src.length; i++) {
-        if (tagBeginIndex === null) {
-          if (src[i] === '<') {
-            tagBeginIndex = i;
-          }
-        } else if (tagEndIndex === null) {
-          if (src[i] === '>') {
-            tagEndIndex = i;
-          }
-        } else {
-          var originalElement = src.substring(tagBeginIndex + 1, tagEndIndex);
-          if (originalElement[0] !== '/') {
-            var elementName = originalElement.substring(0, originalElement.indexOf(' '));
-            var element = originalElement.substring(originalElement.indexOf(' ') + 1);
-            var attributes = element.split(' ');
-            if (attributes.length > 1) {
-              var keyValuePairs = [];
-              var k;
-              for(k = 0; k < attributes.length; k++) {
-                var pair = attributes[k].split('=');
-                var obj = {
-                            'name': pair[0],
-                            'value': pair[1],
-                            'order': options.left.indexOf(pair[0])
-                          };
-                keyValuePairs.push(obj);
-              }
-              keyValuePairs.sort(function (a, b) {
-                return a.order - b.order;
-              });
-              var orderedElement = elementName;
-              var m;
-              for(m = 0; m < keyValuePairs.length; m++) {
-                orderedElement = orderedElement + ' ' + keyValuePairs[m].name + '=' + keyValuePairs[m].value;
-              }
-              src = src.replace(originalElement, orderedElement);
-            }
-          }
-          tagBeginIndex = null;
-          tagEndIndex = null;
-        }
-      }
+      src = reorderAttributes(src, options);
       grunt.file.write(this.files[0].dest, src);
     } else {
       grunt.log.warn('File "' + src + '" does not exist.');
